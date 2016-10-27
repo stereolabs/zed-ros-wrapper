a5eb36ef0e89a1898b895050cacd8c642dad5016
Aymeric Dujardin <aymeric.dujardin@stereolabs.com>
2016-10-27 20:45:09 +0200

    Fix issue with multi cam (zed_id)

--- src/zed_wrapper_node.cpp
+++ src/zed_wrapper_node.cpp
@@ -21,18 +21,17 @@
 #include <ros/ros.h>
 #include <nodelet/loader.h>
 
-
 int main(int argc, char** argv) {
-  ros::init(argc, argv, "zed_wrapper_node");
+    ros::init(argc, argv, "zed_wrapper_node");
 
-  nodelet::Loader nodelet;
-  nodelet::M_string remap(ros::names::getRemappings());
-  nodelet::V_string nargv;
-  nodelet.load(ros::this_node::getName(),
-               "zed_wrapper/ZEDWrapperNodelet",
-               remap, nargv);
+    nodelet::Loader nodelet;
+    nodelet::M_string remap(ros::names::getRemappings());
+    nodelet::V_string nargv;
+    nodelet.load(ros::this_node::getName(),
+            "zed_wrapper/ZEDWrapperNodelet",
+            remap, nargv);
 
-  ros::spin();
+    ros::spin();
 
-  return 0;
+    return 0;
 }
